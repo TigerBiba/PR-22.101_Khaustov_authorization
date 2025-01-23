406720660fd54cbed311f82499edfb07b081e876
Mickey <artplay1995@gmail.com>
2025-01-23 16:59:03 +0700

    Добавьте файлы проекта.

---
+++ PR-22.101_Khaustov_autorization/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using PR_22._101_Khaustov_autorization.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR_22._101_Khaustov_autorization
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void btnAutorization_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (AutorizationEntities db = new AutorizationEntities())
+                {
+                    var staff = db.Staffs.FirstOrDefault(x => x.username == tbLogin.Text && x.password == pswbPassword.Password);
+
+                    if (staff != null)
+                    {
+                        var staffRole = db.Role.FirstOrDefault(x => x.id_role == staff.id_role);
+
+                        MessageBox.Show($"Ваша роль: {staffRole.role_name}");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверный логин или пароль");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ PR-22.101_Khaustov_autorization/Services/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PR_22._101_Khaustov_autorization.Models;
+
+namespace PR_22._101_Khaustov_autorization.Services
+{
+    internal class Helper
+    {
+        AutorizationEntities _context;
+        public AutorizationEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new AutorizationEntities();
+            }
+            return _context;
+        }
+    }
+}
